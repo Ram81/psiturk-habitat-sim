bfb9c32ab12e06a071e666030b0d0e2f6e1821f3
Ram81 <ramramrakhya81@gmail.com>
2020-08-11 01:21:30 +0530

    Intial experiment setup

--- static/js/task.js
+++ static/js/task.js
@@ -5,152 +5,159 @@
  */
 
 // Initalize psiturk object
-var psiTurk = new PsiTurk(uniqueId, adServerLoc, mode);
+window.psiTurk = new PsiTurk(uniqueId, adServerLoc, mode);
 
-var mycondition = condition;  // these two variables are passed by the psiturk server process
-var mycounterbalance = counterbalance;  // they tell you which condition you have been assigned to
-// they are not used in the stroop code but may be useful to you
+//var mycondition = condition;  // these two variables are passed by the psiturk server process
+//var mycounterbalance = counterbalance;  // they tell you which condition you have been assigned to
+// they are not used in this code code yet but may become useful
+
+var steps = [
+  "instructions/instruct-1.html",
+  "viewer"
+];
 
 // All pages to be loaded
 var pages = [
-	"instructions/instruct-1.html",
-	"instructions/instruct-2.html",
-	"instructions/instruct-3.html",
-	"instructions/instruct-ready.html",
-	"stage.html",
-	"postquestionnaire.html",
-	"bindings.html"
+  "instructions/instruct-1.html",
+  "viewer.html",
+  "postquestionnaire.html"
 ];
 
 psiTurk.preloadPages(pages);
-
-var instructionPages = [ // add as a list as many pages as you like
-	"instructions/instruct-1.html",
-	"bindings.html"
-	// "instructions/instruct-2.html",
-	// "instructions/instruct-3.html",
-	// "instructions/instruct-ready.html"
-];
 
 
 /********************
 * HTML manipulation
 *
-* All HTML files in the templates directory are requested 
+* All HTML files in the templates directory are requested
 * from the server when the PsiTurk object is created above. We
-* need code to get those pages from the PsiTurk object and 
+* need code to get those pages from the PsiTurk object and
 * insert them into the document.
 *
 ********************/
 
-/********************
-* STROOP TEST       *
-********************/
-var StroopExperiment = function() {
+/*********************
+* HABITAT TEST       *
+*********************/
+var HabitatExperiment = function() {
 
-	var wordon, // time word is presented
-	    listening = false;
+  var startTime = new Date().getTime();
 
-	// Stimuli for a basic Stroop experiment
-	var stims = [
-			["SHIP", "red", "unrelated"],
-			["MONKEY", "green", "unrelated"],
-			["ZAMBONI", "blue", "unrelated"],
-			["RED", "red", "congruent"],
-			["GREEN", "green", "congruent"],
-			["BLUE", "blue", "congruent"],
-			["GREEN", "red", "incongruent"],
-			["BLUE", "green", "incongruent"],
-			["RED", "blue", "incongruent"]
-		];
+  var response_handler = function(e) {
+    const keyCode = e.keyCode;
 
-	stims = _.shuffle(stims);
+	const rt = new Date().getTime() - startTime;
+    psiTurk.recordTrialData({
+      'phase':"TEST",
+      'keyCode':keyCode,
+      'rt':rt
+    });
+  };
 
-	var next = function() {
-		if (stims.length===0) {
-			finish();
-		}
-		else {
-			stim = stims.shift();
-			show_word( stim[0], stim[1] );
-			wordon = new Date().getTime();
-			listening = true;
-			d3.select("#query").html('<p id="prompt">Type "R" for Red, "B" for blue, "G" for green.</p>');
-		}
-	};
-	
-	var response_handler = function(e) {
-		if (!listening) return;
 
-		var keyCode = e.keyCode,
-			response;
+  psiTurk.recordTrialData({'type':"platformInfo",'navigator':window.navigator});
 
-		switch (keyCode) {
-			case 82:
-				// "R"
-				response="red";
-				break;
-			case 71:
-				// "G"
-				response="green";
-				break;
-			case 66:
-				// "B"
-				response="blue";
-				break;
-			default:
-				response = "";
-				break;
-		}
-		if (response.length>0) {
-			listening = false;
-			var hit = response == stim[1];
-			var rt = new Date().getTime() - wordon;
+  // Load the viewer.html snippet into the body of the page
+  psiTurk.showPage('viewer.html');
+  psiTurk.recordTrialData({'type':"loadViewer",'phase':'TEST'});
 
-			psiTurk.recordTrialData({'phase':"TEST",
-                                     'word':stim[0],
-                                     'color':stim[1],
-                                     'relation':stim[2],
-                                     'response':response,
-                                     'hit':hit,
-                                     'rt':rt}
-                                   );
-			remove_word();
-			next();
-		}
-	};
+  // Register the response handler that is defined above to handle any
+  // key down events.
+  //$("body").focus().keydown(response_handler);
 
-	var finish = function() {
-	    $("body").unbind("keydown", response_handler); // Unbind keys
-	    currentview = new Questionnaire();
-	};
-	
-	var show_word = function(text, color) {
-		d3.select("#stim")
-			.append("div")
-			.attr("id","word")
-			.style("color",color)
-			.style("text-align","center")
-			.style("font-size","150px")
-			.style("font-weight","400")
-			.style("margin","20px")
-			.text(text);
-	};
+  const SimInitialized = function() {
+    return !!(window.demo &&
+              window.demo.task &&
+              window.demo.task.initialized);
+  };
 
-	var remove_word = function() {
-		d3.select("#word").remove();
-	};
+  // Start the test
+  _self = this;
+  _self.iStep = 0;
+  const DoStep = function() {
+    const showViewer = function() {
+      $("#instructions").hide();
+      $("#container").show();
+      if(SimInitialized()) {
+        window.demo.task.bindKeys();
+      }
+    };
+    const showInstructions = function() {
+      $("#instructions").show();
+      $("#container").hide();
+      if(SimInitialized()) {
+        window.demo.task.unbindKeys();
+      }
+    };
+    let step = steps[_self.iStep];
+    if(step instanceof Array) {
+      if(SimInitialized()) {
+        step = step[0];
+      }
+      else {
+        console.log("WARNING: Couldn't determine assistanceCondition. Using 0");
+        step = step[0];
+      }
+    }
+    console.log("iStep:", _self.iStep, "step:", step);
+    window.step = step;
+    psiTurk.recordTrialData({'type':"DoStep",'phase':'TEST','iStep':_self.iStep,'step':step});
+    if(step === "flythrough") {
+      showViewer();
+      const waitForFlythrough = function() {
+        if(SimInitialized()) {
+          window.demo.doFlythrough();
+        } else {
+          console.log("Sim not initialized yet. Waiting");
+          window.setTimeout(waitForFlythrough, 1000);
+        }
+      };
+      waitForFlythrough();
+    } else if(step === "viewer") {
+      // Initialize experiment episode
+      showViewer();
+      //window.demo.doTask();
+    } else {
+      $("#instructions").html(psiTurk.getPage(step))
+      showInstructions();
+      const waitForStartEnable = function() {
+        if(SimInitialized()) {
+          document.getElementById("next").disabled = false;
+        } else {
+          document.getElementById("next").disabled = true;
+          console.log("Sim not initialized yet. Start disabled");
+          window.setTimeout(waitForStartEnable, 1000);
+        }
+      };
+      waitForStartEnable();
+	}
+	$("#next").click(window.finishTrial);
+    $("#prev").click(function() {
+      if(_self.iStep - 1 >= 0) {
+        --_self.iStep;
+        DoStep();
+      }
+    });
+  }
 
-	
-	// Load the stage.html snippet into the body of the page
-	psiTurk.showPage('stage.html');
+  window.finishTrial = function(doReset = false) {
+      psiTurk.recordTrialData({'type':"finishStep",'phase':'TEST'});
+      ++_self.iStep;
 
-	// Register the response handler that is defined above to handle any
-	// key down events.
-	$("body").focus().keydown(response_handler); 
+      if(_self.iStep < steps.length) {
+        if(doReset && SimInitialized()) {
+          window.demo.task.reset();
+        }
 
-	// Start the test
-	next();
+        DoStep();
+      } else {
+        if(SimInitialized())
+          window.demo.task.unbindKeys();
+        //$("body").unbind("keydown"); // Unbind keys
+        window.currentview = new Questionnaire();
+      }
+  };
+  DoStep();
 };
 
 
@@ -160,77 +167,117 @@ var StroopExperiment = function() {
 
 var Questionnaire = function() {
 
-	var error_message = "<h1>Oops!</h1><p>Something went wrong submitting your HIT. This might happen if you lose your internet connection. Press the button to resubmit.</p><button id='resubmit'>Resubmit</button>";
+  var error_message = "<h1>Oops!</h1><p>Something went wrong submitting your HIT. This might happen if you lose your internet connection. Press the button to resubmit.</p><button id='resubmit'>Resubmit</button>";
 
-	record_responses = function() {
+  const check_responses = function() {
+    let responsesFilled = true;
+    $('textarea').each( function(i, val) {
+      if(this.value === "")
+        responsesFilled = false;
+    });
+    $('select').each( function(i, val) {
+      if(this.value === "")
+        responsesFilled = false;
+    });
+    let radioNames = new Set();
+    let checkedRadioNames = new Set();
+    $('input').each( function(i, val) {
+      const name = (this.id && this.id !== "")?this.id:this.name;
+      if(val.type === "radio") {
+        radioNames.add(name);
+        // Only record checked radio buttons
+        if(!val.checked)
+          return;
+        else {
+          checkedRadioNames.add(name);
+        }
+      }
+      if(this.value === "")
+        responsesFilled = false;
 
-		psiTurk.recordTrialData({'phase':'postquestionnaire', 'status':'submit'});
+      psiTurk.recordUnstructuredData(name, this.value);
+    });
+    return responsesFilled && checkedRadioNames.size === radioNames.size;
 
-		$('textarea').each( function(i, val) {
-			psiTurk.recordUnstructuredData(this.id, this.value);
-		});
-		$('select').each( function(i, val) {
-			psiTurk.recordUnstructuredData(this.id, this.value);		
-		});
+  };
+  const record_responses = function() {
 
-	};
+    psiTurk.recordTrialData({'phase':'postquestionnaire', 'status':'submit'});
 
-	prompt_resubmit = function() {
-		document.body.innerHTML = error_message;
-		$("#resubmit").click(resubmit);
-	};
+    $('textarea').each( function(i, val) {
+      psiTurk.recordUnstructuredData(this.id, this.value);
+    });
+    $('select').each( function(i, val) {
+      psiTurk.recordUnstructuredData(this.id, this.value);
+    });
+    $('input').each( function(i, val) {
+      // Only record checked radio buttons
+      if(val.type === "radio" && !val.checked)
+        return;
+      if(this.id && this.id !== "")
+        psiTurk.recordUnstructuredData(this.id, this.value);
+      else
+        psiTurk.recordUnstructuredData(this.name, this.value);
+    });
+  };
 
-	resubmit = function() {
-		document.body.innerHTML = "<h1>Trying to resubmit...</h1>";
-		reprompt = setTimeout(prompt_resubmit, 10000);
-		
-		psiTurk.saveData({
-			success: function() {
-			    clearInterval(reprompt); 
-                psiTurk.computeBonus('compute_bonus', function(){
-                	psiTurk.completeHIT(); // when finished saving compute bonus, the quit
-                }); 
+  prompt_resubmit = function() {
+    document.body.innerHTML = error_message;
+    $("#resubmit").click(resubmit);
+  };
 
+  resubmit = function() {
+    document.body.innerHTML = "<h1>Trying to resubmit...</h1>";
+    reprompt = setTimeout(prompt_resubmit, 10000);
 
-			}, 
-			error: prompt_resubmit
-		});
-	};
+    psiTurk.saveData({
+      success: function() {
+          clearInterval(reprompt);
+          psiTurk.completeHIT(); // when finished saving compute bonus, the quit
+      },
+      error: prompt_resubmit
+    });
+  };
 
-	// Load the questionnaire snippet 
-	psiTurk.showPage('postquestionnaire.html');
-	psiTurk.recordTrialData({'phase':'postquestionnaire', 'status':'begin'});
-	
-	$("#next").click(function () {
-	    record_responses();
-	    psiTurk.saveData({
+  // Load the questionnaire snippet
+  psiTurk.showPage('postquestionnaire.html');
+  psiTurk.recordTrialData({'phase':'postquestionnaire', 'status':'begin'});
+
+  $("#next").click(function () {
+    if(check_responses()) {
+      record_responses();
+      psiTurk.saveData({
             success: function(){
-                psiTurk.computeBonus('compute_bonus', function() { 
-                	psiTurk.completeHIT(); // when finished saving compute bonus, the quit
-                }); 
-            }, 
+                psiTurk.completeHIT(); // when finished saving compute bonus, the quit
+            },
             error: prompt_resubmit});
-	});
-    
-	
+    }
+    else {
+      const msg = "Please answer all the questions!";
+      const status = document.getElementById("questionnairestatus");
+      if(status) {
+        status.style = "color:red";
+        status.innerHTML = msg;
+      }
+      else
+        console.warn(msg);
+    }
+  });
+
+
 };
 
 // Task object to keep track of the current phase
-var currentview;
-window.config = {
-	enablePhysics: true,
-	defaultPhysConfig: "default.phys_scene_config.json",
-	scene:  "skokloster-castle.glb"
-}
-// window.config.enablePhysics = true;
-// window.config.defaultPhysConfig = "default.phys_scene_config.json";
+window.currentView = undefined;
 
 /*******************
  * Run Task
  ******************/
 $(window).load( function(){
-    psiTurk.doInstructions(
-    	instructionPages, // a list of pages you want to display in sequence
-    	function() { currentview = new StroopExperiment(); } // what you want to do when you are done with instructions
-    );
+   window.currentView = new HabitatExperiment(); ;
+
+    /*psiTurk.doInstructions(
+      instructionPages, // a list of pages you want to display in sequence
+      function() { window.currentView = new HabitatExperiment(); } // what you want to do when you are done with instructions
+    );*/
 });
